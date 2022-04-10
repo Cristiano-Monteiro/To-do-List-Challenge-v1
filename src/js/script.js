c5db107d88c7abf272b283ac075d30019d67d654
Cristiano da Silva Monteiro <cristianodasilvamonteiro@gmail.com>
2022-04-09 23:58:45 -0300

    Improving script.js and style.css

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,15 @@
-let btnAdd = document.getElementsByClassName("btnAdd");
-let taskInput = document.getElementById("taskInput");
-let ul = document.getElementsByTagName("ul");
+let list = document.querySelector("ul");
+let ButtonAdd = document.getElementById("buttonAdd");
+
+ButtonAdd.addEventListener("click", addTask);
+
+function addTask(event){
+    let TaskInput = document.getElementById("taskInput");
+    let ValueInput = TaskInput.value;
+    const ItemList = document.createElement("li");
+    ItemList.innerText = ValueInput;
+    const ButtonRemove = document.createElement("button");
+    ButtonRemove.innerText = "Remove";
+    ItemList.appendChild(ButtonRemove); 
+    list.appendChild(ItemList);
+};
